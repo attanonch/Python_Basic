e3784c0b3020368c740904641ff562f748424cf8
ATTANON CHUPOOL <attanonchupool@gmail.com>
2024-06-04 20:50:44 +0700

    update

---
+++ test.py
@@ -0,0 +1,11 @@
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+allset = {}
+
+allset.update(set1)
+
+print(set1)
+print(set2)
+print(set3)
+print(allset)
